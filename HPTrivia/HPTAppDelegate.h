d3446f55067f88d92da92794124e7bfd910f210d
James Hicklin <hickin.james@gmail.com>
2013-08-20 13:28:44 -0700

    Initial Commit

---
+++ HPTrivia/HPTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HPTAppDelegate.h
+//  HPTrivia
+//
+//  Created by James Hicklin on 2013-08-20.
+//  Copyright (c) 2013 James Hicklin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HPTrivia/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HPTrivia
+//
+//  Created by James Hicklin on 2013-08-20.
+//  Copyright (c) 2013 James Hicklin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HPTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([HPTAppDelegate class]));
+  }
+}
