54300b360dde0d1fd238adde3289b0ad8d2034ae
James Hicklin <hickin.james@gmail.com>
2013-08-26 08:07:03 -0700

    Added a settings view

--- HPTrivia/SettingsViewController.h
+++ HPTrivia/SettingsViewController.h
@@ -1,17 +1,15 @@
 //
-//  MainMenuViewController.h
+//  SettingsViewController.h
 //  HPTrivia
 //
-//  Created by James Hicklin on 2013-08-20.
+//  Created by James Hicklin on 2013-08-26.
 //  Copyright (c) 2013 James Hicklin. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface MainMenuViewController : UIViewController
-
+@interface SettingsViewController : UIViewController
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
 @property (strong, nonatomic) IBOutlet UILabel *titleLabel;
-@property (strong, nonatomic) IBOutlet UIButton *settingsButton;
 
 @end
--- HPTrivia/SettingsViewController.m
+++ HPTrivia/SettingsViewController.m
@@ -1,22 +1,22 @@
 //
-//  MainMenuViewController.m
+//  SettingsViewController.m
 //  HPTrivia
 //
-//  Created by James Hicklin on 2013-08-20.
+//  Created by James Hicklin on 2013-08-26.
 //  Copyright (c) 2013 James Hicklin. All rights reserved.
 //
 
-#import "MainMenuViewController.h"
+#import "SettingsViewController.h"
 
-@interface MainMenuViewController ()
+@interface SettingsViewController ()
 
 @end
 
-@implementation MainMenuViewController
-
-#define IS_PHONEPOD5() ([UIScreen mainScreen].bounds.size.height == 568.0f && [UIScreen mainScreen].scale == 2.f && UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+@implementation SettingsViewController
 
 @synthesize imageView;
+
+#define IS_PHONEPOD5() ([UIScreen mainScreen].bounds.size.height == 568.0f && [UIScreen mainScreen].scale == 2.f && UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -31,14 +31,13 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+  
   if(IS_PHONEPOD5()) {
     imageView.image = [UIImage imageNamed:@"parchment.png"];
   } else {
     imageView.image = [UIImage imageNamed:@"parchment-568h@2x.png"];
   }
-  
   [self.titleLabel setFont:[UIFont fontWithName:@"ParryHotter" size:50]];
-  
 }
 
 // Hides status bar
@@ -51,10 +50,6 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-// Declaration is important here. It signals an unwind segue.
--(IBAction)unwindToMainMenu:(UIStoryboardSegue*)segue {
 }
 
 @end
